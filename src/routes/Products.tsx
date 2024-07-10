647f75dd29f173244a1853aaefe228c6a8f52a01
Ruhtra <kawanarthurskate@gmail.com>
2024-07-10 17:03:51 -0300

    Adicionada novas rotas

--- src/routes/Products.tsx
+++ src/routes/Products.tsx
@@ -1,19 +1,33 @@
 import { Loading } from "../components/Loading/Loading"
-import { useGetAllProduct } from "../services/Querys/Product/Product"
+import { useCreateProduct, useGetAllProduct } from "../services/Querys/Product/Product"
 
 export function Products() {
     const { data: products, status: productsStatus } = useGetAllProduct()
 
+    const { mutate, error } = useCreateProduct()
+
     if (productsStatus == "loading")  return <Loading />
     if (productsStatus == "error") return <h2>Houve um erro ao carregar produtos</h2> 
+
+  
+
+    function added () {
+        mutate({
+            name: "onion",
+            price: 3.2
+        })
+    }
 
     return (
         <>
             {
                 products?.map(p => {
-                    return <p>{JSON.stringify(p)}</p>
+                    return <p key={p.id}>{JSON.stringify(p)}</p>
                 })
             }
+
+            <button onClick={added}>added onion</button>
+
 
         </>
     )
--- src/services/Querys/Product/Product.ts
+++ src/services/Querys/Product/Product.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "react-query";
-import { GetAllProductResponseDto } from "./ProductDto";
+import { useMutation, useQuery } from "react-query";
 import { api } from "../../Api";
+import { CreateProductRequestDto, GetAllProductResponseDto, GetProductRequestDto, GetProductResponseDto } from "./ProductDto";
+import { queryClient } from "../../QueryClient";
 
 export function useGetAllProduct() {
     return useQuery<GetAllProductResponseDto[]>({
@@ -11,4 +12,32 @@ export function useGetAllProduct() {
         },
         staleTime: 1000 * 60
     })
+}
+
+export function useGetProduct({ id }: GetProductRequestDto) {
+    return useQuery<GetProductResponseDto[]>({
+        queryKey: ['Product', id],
+        queryFn: async () => {
+            const response = await api.get<GetProductResponseDto[]>('/product/get', {
+                params: {
+                    id: id
+                }
+            })
+            return response.data
+        },
+        staleTime: 1000 * 60
+    })
+}
+
+
+export function useCreateProduct() {
+    return useMutation(
+        async (product: CreateProductRequestDto) => {
+            await api.post('/product/create', product)
+        }, {
+            onSuccess: () => {
+                queryClient.invalidateQueries('AllProducts')
+            }
+        }
+    )
 }
