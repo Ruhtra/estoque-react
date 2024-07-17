22e79d303d0b450536f93d9f621c9f4695a00225
Ruhtra <kawanarthurskate@gmail.com>
2024-07-17 02:36:46 -0300

    Added CreatedAt in table history

--- src/routes/HistoryRoute/HistoryRoute.tsx
+++ src/routes/HistoryRoute/HistoryRoute.tsx
@@ -17,14 +17,18 @@ export function HistoryRoute() {
                     <span>amount</span>
                     <span>price</span>
                     <span>Operation</span>
+                    <span>DateTime</span>
                 </li>
                 {
                     data?.map(e => {
+                        let date = new Date(e.createdAt)
+
                         return <li key={e.id}>
                             <span>{e.product.name}</span>
                             <span>{e.amount}</span>
                             <span>{e.price}</span>
                             <span>{e.operation}</span>
+                            <span>{date.toLocaleDateString()}</span>
                         </li>
                     })
                 }
--- src/services/Querys/History/HistoryDto.ts
+++ src/services/Querys/History/HistoryDto.ts
@@ -3,11 +3,17 @@ export enum OperationHistoryEnum {
     decrease = 'decrease'
 }
 
+export type GetAllHistoryRequestDto = {
+    operation?: OperationHistoryEnum
+}
+
+
 export type GetAllHistoryResponseDto = {
     id: string
     amount: number
     price: number
-    operation: OperationHistoryEnum
+    operation: OperationHistoryEnum,
+    createdAt: Date,
     product: {
         name: string
     }
